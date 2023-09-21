4cd871c00b3624294bad9bcafbe1ce5b16ed3420
NavinAananthan <navinaananthan63@gmail.com>
2023-09-21 12:28:22 +0530

    logistic-regression

--- Regression/Logistic_Regression.py
+++ Regression/Logistic_Regression.py
@@ -1,81 +1,77 @@
 import torch
 import torch.nn as nn
 import numpy as np
-from sklearn.datasets import make_classification
+from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import matplotlib.pyplot as plt
 
 class LogisticRegressionModel(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self, n_input_features):
         super(LogisticRegressionModel, self).__init__()
-        self.linear = nn.Linear(input_size, 1)
+        self.linear = nn.Linear(n_input_features, 1)
 
     def forward(self, x):
-        return torch.sigmoid(self.linear(x))
+        y_pred = torch.sigmoid(self.linear(x))
+        return y_pred
 
 class LogisticRegression:
-    def __init__(self, learning_rate, num_epochs, X, Y):
-        self.learning_rate = learning_rate
+    def __init__(self, num_epochs=100, learning_rate=0.01):
         self.num_epochs = num_epochs
+        self.learning_rate = learning_rate
+        self.model = None
+        self.criterion = None
+        self.optimizer = None
+        self.scaler = None
 
-        # Creating a synthetic dataset
-        self.X = torch.from_numpy(X.astype(np.float32))
-        self.Y = torch.from_numpy(Y.astype(np.float32))
-        self.n_samples, self.n_features = self.X.shape
+    def preprocess_data(self, X, y):
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
 
-        # Initialize the model, loss, and optimizer
-        self.model = LogisticRegressionModel(self.n_features)
-        self.loss = nn.BCELoss()
+        self.scaler = StandardScaler()
+        X_train = self.scaler.fit_transform(X_train)
+        X_test = self.scaler.transform(X_test)
+
+        X_train = torch.from_numpy(X_train.astype(np.float32))
+        X_test = torch.from_numpy(X_test.astype(np.float32))
+        y_train = torch.from_numpy(y_train.astype(np.float32))
+        y_test = torch.from_numpy(y_test.astype(np.float32))
+
+        y_train = y_train.view(y_train.shape[0], 1)
+        y_test = y_test.view(y_test.shape[0], 1)
+
+        return X_train, X_test, y_train, y_test
+
+    def train(self, X_train, y_train):
+        n_samples, n_features = X_train.shape
+        self.model = LogisticRegressionModel(n_features)
+        self.criterion = nn.BCELoss()
         self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.learning_rate)
 
-    def train(self):
         for epoch in range(self.num_epochs):
-            # Forward pass and loss
-            y_pred = self.model(self.X).squeeze()
-            l = self.loss(y_pred, self.Y)
-            
-            # Backward pass
-            l.backward()
-            
-            # Update weights
+            y_pred = self.model(X_train)
+            loss = self.criterion(y_pred, y_train)
+
+            loss.backward()
             self.optimizer.step()
             self.optimizer.zero_grad()
 
             if (epoch + 1) % 10 == 0:
-                print(f'epoch: {epoch + 1}, loss: {l.item():.4f}')
+                print(f'Epoch: {epoch + 1}, Loss: {loss.item():.4f}')
 
-    def predict(self, X):
+    def evaluate(self, X_test, y_test):
         with torch.no_grad():
-            y_pred = self.model(X)
-            y_pred_cls = (y_pred > 0.5).float()
-        return y_pred_cls
+            y_predicted = self.model(X_test)
+            y_predicted_cls = y_predicted.round()
+            acc = y_predicted_cls.eq(y_test).sum().item() / float(y_test.shape[0])
+            return acc
 
-    def plot_decision_boundary(self):
-        X_numpy = self.X.numpy()
-        Y_numpy = self.Y.numpy()
-        x_min, x_max = X_numpy[:, 0].min() - 1, X_numpy[:, 0].max() + 1
-        y_min, y_max = X_numpy[:, 1].min() - 1, X_numpy[:, 1].max() + 1
-        xx, yy = np.meshgrid(np.arange(x_min, x_max, 0.01), np.arange(y_min, y_max, 0.01))
-        grid_tensor = torch.from_numpy(np.c_[xx.ravel(), yy.ravel()].astype(np.float32))
-        Z = self.predict(grid_tensor)
-        Z = Z.view(xx.shape)
-        plt.contourf(xx, yy, Z.numpy(), cmap=plt.cm.Spectral)
-        plt.scatter(X_numpy[:, 0], X_numpy[:, 1], c=Y_numpy, cmap=plt.cm.Spectral)
-        plt.show()
+if __name__ == '__main__':
+    bc = datasets.load_breast_cancer()
+    X, y = bc.data, bc.target
 
+    lr_model = LogisticRegression(num_epochs=100, learning_rate=0.01)
+    X_train, X_test, y_train, y_test = lr_model.preprocess_data(X, y)
 
-# Instantiate and train the model
-X, Y = make_classification(n_samples=100, n_features=2, n_classes=2, n_clusters_per_class=1, n_redundant=0, random_state=42)
-learning_rate = 0.01
-num_epochs = 200
-logistic_regression = LogisticRegression(learning_rate, num_epochs, X, Y)
-logistic_regression.train()
+    lr_model.train(X_train, y_train)
 
-input_value = torch.tensor([[1.0,10.0]])
-predicted_probabilities = logistic_regression.predict(input_value)
-predicted_class = (predicted_probabilities > 0.5).float().item()
-
-print(predicted_class)
-
-# Plot the decision boundary
-#logistic_regression.plot_decision_boundary()
+    accuracy = lr_model.evaluate(X_test, y_test)
+    print(f'Accuracy: {accuracy:.4f}')
